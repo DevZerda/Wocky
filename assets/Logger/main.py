ab330c2a15f9d36af1dc7918043a6a70345e2779
Zerda <74839733+DevZerda@users.noreply.github.com>
2021-06-12 20:56:46 -0400

    Create main.py

---
+++ assets/Logger/main.py
@@ -0,0 +1,61 @@
+## Modules
+import os, sys, time
+
+## Files
+from ..Config.main import *
+from ..utils.main import *
+from .discord import *
+
+class MainLogger:
+    def Log(logType, notification):
+        logResp = "Log: " + logType + " | Time: " + str(utils.CurrentTime()) + "\n"
+        if "CMD" in logType:
+            logResp += "User: " + Current.CurrentInfo['Username'] + " | IP: " + Current.CurrentInfo['IP'] + "\n"
+            logResp += "Level: " + str(Current.CurrentInfo['Level']) + " | Admin: " + str(Current.CurrentInfo['Admin']) + "\n"
+            logResp += "Cmd: " + Current.CurrentCmd['Cmd'] + " | Full CMD: " + Current.CurrentCmd['fullcmd'] + "\n"
+        else:
+            logResp = logType
+        print(logResp)
+
+        LogTypes.LogCommand(logResp)
+        if logType == "attack":
+            LogTypes.LogAttack(logResp)
+        elif "login: " in logType:
+            LogTypes.LogLogin(logResp)
+        elif logType == "status":
+            LogTypes.Log
+
+        Discord.send_logs(logResp)
+        if notification == True:
+            LogTypes.sendDiscord(logType, logResp)
+
+
+
+class LogTypes:
+
+    def sendDiscord(Type, msg):
+        if Type == "attack":
+            Discord.send_attack(msg)
+        elif Type == "status":
+            Discord.send_status(msg)
+        elif "login" in Type:
+            Discord.send_login(msg)
+
+    def LogCommand(logThis):
+        logsDB = open(os.getcwd() + "/assets/db/logs.db", "a")
+        logsDB.write(logThis)
+        logsDB.close()
+
+    def LogAttack(logThis):
+        attkDB = open("./assets/db/attacks.db", "a")
+        attkDB.write(logThis)
+        attkDB.close()
+        
+
+    def LogLogin(logThis):
+        LoginDB = open("./assets/db/logins.db", "a")
+        LoginDB.write(logThis)
+        LoginDB.close()
+
+
+    
