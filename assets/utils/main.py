0d643190885497d5ff17d45e100bcac6b0cac5b8
Zerda <74839733+DevZerda@users.noreply.github.com>
2021-06-12 21:09:22 -0400

    Create main.py

---
+++ assets/utils/main.py
@@ -0,0 +1,56 @@
+# from Quantum_setup import GetOS
+import os, sys, time, platform, datetime
+
+class utils:
+    def set_Title(socket, msg):
+        socket.send(f"\033]0;{msg}\007".encode())
+
+    def CurrentTime(): # This is date and time not just time !
+        return datetime.datetime.now()
+
+    def CheckForPython3Unix():
+        if GetOS() == True:
+            if os.path.isfile("/usr/bin/python3"):
+                return True
+            else:
+                return False
+
+    def FlashingCursor(socket):
+        while(True):
+            socket.send("\033[?25l".encode())
+            time.sleep(1)
+            socket.send("\033[?25h\033[?0c".encode())
+            time.sleep(1)
+
+    def changeMOTD(new_motd):
+        motd = open("./assets/db/motd.db", "w")
+        motd.write(new_motd)
+        motd.close()
+        return f"MOTD Successfully changed to ({new_motd})"
+
+    def GetMOTD():
+        motd = open("./assets/db/motd.db", "r").read()
+        return motd
+
+
+class OS_Func:
+    def GetOSType():
+        if "Windows" in platform.platform():
+            return False
+        elif "Linux" in platform.platform():
+            return True
+        else:
+            print("[x] Error, This net does not support this OS")
+            exit()
+
+class arrUtils:
+    def arr2str(arr, between):
+        if isinstance(arr, list) == False:
+            print("[x] Error, Invalid value provided!")
+            exit(0)
+        n_str = ""
+        for u in arr:
+            n_str += u + between
+
+        return n_str
+
