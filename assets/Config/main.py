0136d2177847566a7765d4dc32bab3bcadbda46c
Zerda <74839733+DevZerda@users.noreply.github.com>
2021-06-12 20:29:18 -0400

    Create main.py

---
+++ assets/Config/main.py
@@ -0,0 +1,49 @@
+
+class ServerConfig:
+	clients = []
+	bots = []
+
+class Strings:
+
+	appInfo = {
+		"appCreator": "Zerda",
+		"appOwner": "Exo",
+		"appDevs": "Zerda, Exo, clever, Max, Beta",
+		"appAdmins": "Pressure",
+		"appVersion": "1.00"
+	}
+
+	def hostname(name):
+		addName = "Net"
+		if name:
+			addName = name
+		
+		host = "\r\x1b[33m[\x1b[31mQuantum\x1b[33m@\x1b[31m" + addName + "\x1b[33m]══► $ "
+		return host
+
+	MainColors = {
+		"Red": "\x1b[31m",
+		"Yellow": "\x1b[33m",
+		"Blue": "\x1b[34m",
+		"Purple": "\x1b[35m",
+		"Green": "\x1b[32m",
+		"Cyan": "\x1b[36m",
+		"Black": "\x1b[30m",
+		"Grey": "\x1b[37m",
+		"White": "\x1b[37m",
+		"Reset": "\x1b[39m",
+		"Background_Black": "\x1b[40m",
+		"Background_Red": "\x1b[41m",
+		"Background_Green": "\x1b[42m",
+		"Background_Yellow": "\x1b[43m",
+		"Background_Blue": "\x1b[44m",
+		"Background_Purple": "\x1b[45m",
+		"Background_Cyan": "\x1b[46m",
+		"Background_LightGrey": "\x1b[47m",
+		"Background_DarkGrey": "\x1b[100m",
+		"Background_LightRed": "\x1b[101m",
+		"Background_LightGreen": "\x1b[102m",
+		"Background_LightYellow": "\x1b[103m",
+		"Background_Reset": "\x1b[49m",
+		"Clear": "\033[2J\033[1;1H"
+	}
