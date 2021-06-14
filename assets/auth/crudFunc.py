d4e558d528a721f789044b8e7f5cc965f0b9606e
Zerda <74839733+DevZerda@users.noreply.github.com>
2021-06-14 14:55:36 -0400

    Update crudFunc.py

--- assets/auth/crudFunc.py
+++ assets/auth/crudFunc.py
@@ -2,6 +2,16 @@ import os, sys, time
 from .crud import *
 
 class CrudFunctions:
+    def MyStats(user):
+        info = CRUD.GetUser(user).split(",")
+        r = f"User: {info[0]}\n"
+        r += f"IP: {info[1]}\n"
+        r += f"Level: {info[3]}\n"
+        r += f"Maxtime: {info[4]}\n"
+        r += f"Admin: {info[5]}\n"
+        return r
+
+
     def TokenValidation(rtoken):
         return ""
         
@@ -9,6 +19,7 @@ class CrudFunctions:
         info = CRUD.GetUser(user).split(",")
         CRUD.RemoveUser(user)
         CRUD.CreateUser(info[0], newpw, info[3], info[4], info[5])
+        return "Password successfully changed!\r\n"
 
     def isRegistered(username):
         info = CRUD.GetUser(username)
