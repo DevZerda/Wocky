d91a4b5587d5ea8aa9d22d0c712471bc19fd1c37
Zerda <74839733+DevZerda@users.noreply.github.com>
2021-06-15 18:08:14 -0400

    Create testing.py

---
+++ testing.py
@@ -0,0 +1,47 @@
+import os, sys, time
+
+Banner = open("lol.txt", "r").read()
+
+output_lines = Banner.split("\n")
+
+for_loop = False
+send_for_loop = False
+Line_Count = 0
+
+output_for_loop = ""
+for line in output_lines:
+    if line.startswith("for("):
+        for_int = line.split("(")[1].split(")")[0]
+        for_loop = True
+        new_c = Line_Count + 1
+        while(True):
+            if output_lines[new_c] == "}":
+                for_loop = False
+            else:
+                output_for_loop += output_lines[new_c] + "\r\n"
+
+            if for_loop == False:
+                send_for_loop = True
+                break
+            new_c += 1
+        if send_for_loop == True:
+            for i in range(1,int(for_int)):
+                output_for_loop = output_for_loop.replace("{CLEAR}", "\033[2J\033[1;1H")
+                output_for_loop = output_for_loop.replace("{RED}", "\x1b[31m")
+                output_for_loop = output_for_loop.replace("{RESET}", "\x1b[39m")
+                print(output_for_loop)
+            output_for_loop = ""
+    elif line == "}":
+        pass
+    elif "SLEEP(" in line:
+        get_int = line.split("(")[1].replace(")", "")
+        time.sleep(int(get_int))
+    else:
+        line = line.replace("{CLEAR}", "\033[2J\033[1;1H")
+        line = line.replace("{RED}", "\x1b[31m")
+        line = line.replace("{RESET}", "\x1b[39m")
+        # print("Line Count: " + str(Line_Count) + " | " + line)
+        print(line)
+    Line_Count += 1
+
+            
