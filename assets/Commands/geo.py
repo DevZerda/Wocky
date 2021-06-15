61efefb5c078854f47f42383350c9e323796a704
Zerda <74839733+DevZerda@users.noreply.github.com>
2021-06-15 16:40:29 -0400

    Create geo.py

---
+++ assets/Commands/geo.py
@@ -0,0 +1,22 @@
+import os
+import sys
+import time
+import requests
+import json
+
+
+def geo_command(socket, argv):
+    """
+    Display geo location from an IP address
+
+    :param socket: socket from cnc
+    :param argv: arguments from cmd
+    """
+    if len(argv) == 2:
+        jsonResp = (requests.get(
+            "http://extreme-ip-lookup.com/json/" + argv[1]).text)
+        fix = jsonResp.replace("\",\"", "\r\n")
+        fix = fix.replace("\"", "").replace("{", "").replace("}", "")
+        socket.send(f"{fix}\r\n".encode(""))
+    else:
+        socket.send("[x] Error, Invalid Arugment\r\nUsage: geo <ip>\r\n".encode())
