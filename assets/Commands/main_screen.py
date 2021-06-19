498ab0848c035f1cd508c1f8c3e056a46ff2debf
n4n0 <74839733+DevZerda@users.noreply.github.com>
2021-06-19 16:31:28 -0400

    Update main_screen.py

--- assets/Commands/main_screen.py
+++ assets/Commands/main_screen.py
@@ -3,6 +3,7 @@ import os, sys, time, random
 
 # Files
 from ..auth.main import *
+from ..auth.crud import *
 from ..Logger.main import *
 from ..banner_system.modify import *
 from ..Config.main import *
@@ -14,44 +15,50 @@ def MainScreen(socket, addr):
     socket.send(str(Strings.MainColors['Clear'] +  BannerModify.GetBannerFromFile("main_screen")).encode())
     socket.send("                        Choose an option: ".encode())
     option_dude = socket.recv(buffer_length).decode().strip().replace("\r\n", "")
-    if int(option_dude) == 1:
+    if int(option_dude) == 1 or option_dude.lower() == "login":
         login(socket,addr)
-    elif int(option_dude) == 2:
+    elif int(option_dude) == 2 or option_dude.lower() == "register":
         register(socket,addr)
-    elif int(option_dude) == 3:
-        ServerConfig.clients.append(["Guest" + str(random.randint(0, 99999)), socket, addr[0], addr[1]])
-        WockyChat(socket, addr)
-    elif int(option_dude) == 4:
-        about(socket)
+    # elif int(option_dude) == 3 or option_dude == "chatroom":
+    #     ServerConfig.clients.append(["Guest" + str(random.randint(0, 99999)), socket, addr[0], addr[1]])
+    #     WockyChat(socket, addr)
+    # elif int(option_dude) == 4:
+        # about(socket)
     else:
-        socket.send("[x] Error, Invalid Argument\r\n".encode())
-        MainScreen(socket,addr)
+        socket.send("[x] Error, Wrong option!\r\n".encode())
+        socket.close()
 
 
 
 def login(socket, addr):
-    socket.send(str(Strings.MainColors['Clear']).encode())
-    # User Input Login Section
-    socket.send("\rUsername: ".encode())
-    username = socket.recv(buffer_length).decode().strip().replace("\r\n", "")
-    socket.recv(512)
-    socket.send("\rPassword: ".encode())
-    socket.recv(512)
-    password = socket.recv(buffer_length).decode().strip().replace("\r\n", "")
+    if NetSettings.login == True:
+        socket.send(str(Strings.MainColors['Clear']).encode())
+        # User Input Login Section
+        socket.send("\rUsername: ".encode())
+        username = socket.recv(buffer_length).decode().strip().replace("\r\n", "")
+        if username == "" or username == None:
+            username = socket.recv(buffer_length).decode().strip().replace("\r\n", "")
+        socket.recv(512)
+        socket.send(str(f"\rPassword: {Strings.MainColors['Black']}").encode())
+        password = socket.recv(buffer_length).decode().strip().replace("\r\n", "")
 
-    print(f"{username} | {password}") #Debugging
+        print(f"{username} | {password}") #Debugging
 
-    # Login Check
-    if "[+]" in Auth.Login(username, password, addr[0]): # This is a weird way of authentication lol 
-        Strings.CurrentUser = username
-        Strings.CurrentIP = addr[0]
-        socket.send(str(Strings.MainColors['Clear'] + CustomBannerMaker.CreateMOTD(utils.GetMOTD()) + BannerModify.GetBannerFromFile("main") + "\r\n").encode())
-        socket.send(f"Welcome to Quantum Net {username}\r\n".encode())
-        MainLogger.Log(f"login: {username} | Time: {utils.CurrentTime()}", True)
-        ServerConfig.clients.append([username, socket, addr[0], addr[1]])
+        # Login Check
+        if "[+]" in Auth.Login(username, password, addr[0]): # This is a weird way of authentication lol 
+            Strings.CurrentUser = username
+            Strings.CurrentIP = addr[0]
+            userinfo = CRUD
+            socket.send(str(Strings.MainColors['Clear'] + CustomBannerMaker.CreateMOTD(utils.GetMOTD()) + BannerModify.GetBannerFromFile("main") + "\r\n").encode())
+            socket.send(f"Welcome to Quantum Net {username}\r\n".encode())
+            MainLogger.Log(f"login: {username} | Time: {utils.CurrentTime()}", True)
+            ServerConfig.clients.append([username, socket, addr[0], addr[1]])
+        else:
+            socket.send(f"{Strings.MainColors['Reset']}[x] Error, Incorrect username or password. Try again....".encode())
+            time.sleep(4)
+            socket.close()
     else:
-        socket.send("[x] Error, Incorrect username or password. Try again....".encode())
-        time.sleep(4)
+        socket.send("[x] Error, Wocky NET login is currently disabled for maintenance, try again later....\r\n".encode())
         socket.close()
 
 def register(socket, addr): ##) Function not finished
