d60132c63ad67a1c719b6bc124f6fb1eeffcc5b8
n4n0 <74839733+DevZerda@users.noreply.github.com>
2021-06-26 19:15:29 -0400

    Update nn.py

--- nn.py
+++ nn.py
@@ -4,4 +4,6 @@ from assets.api_attack_system.main import *
 
 # print(APICrud.listAPIs())
 
-print(APICrud.GetAPI("SST"))
+# print(APICrud.GetAPI("SST"))
+
+print(APICrud.GetAPIWithMethod(sys.argv[1]))
