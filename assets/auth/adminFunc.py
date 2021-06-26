e0e4d6fe7ad7c21b2ce0e7286a6d4c08448d85d8
n4n0 <74839733+DevZerda@users.noreply.github.com>
2021-06-26 09:53:54 -0400

    Update adminFunc.py

--- assets/auth/adminFunc.py
+++ assets/auth/adminFunc.py
@@ -10,24 +10,41 @@ class AdminFunc:
         userline = ""
         db = open("./assets/db/users.db", "r").read()
         users = db.split("\n")
-        userline += "Username    IP   Level   Maxtime Conn  On-Going  Admin\r\n_____________________________________________________________________\r\n"
+        userline += "Username      IP         Level   Maxtime Conn  On-Going  Admin\r\n_____________________________________________________________________\r\n"
         for user in users:
             if len(user) > 5:
                 fix = user.replace("('", "").replace("')", "").replace("','", ",")
                 fix2 = user.replace("('", "").replace("')", "").split("','")
-                userline += f"{fix2[0]}\t   {fix2[1]}\t   {fix2[3]}\t   {fix2[4]}\t   {fix2[5]}\t   {fix2[6]}\t   {fix2[7]}\r\n"
+                userline += f"{fix2[0]}\t     {fix2[1]}\t  {fix2[3]}\t   {fix2[4]}\t   {fix2[5]}\t   {fix2[6]}\t   {fix2[7]}\r\n"
                 # print(fix)
         return userline
 
     def kick_user(socket, user):
-        f = 0
-        for u in ServerConfig.clients:
-            if u[0] == user:
-                u[1].send(str(Strings.MainColors['Clear'] + Strings.MainColors['Red'] + "\r\n\r\n\r\n\r\n\r\n\r\nYou have been disconnected by an admin!").encode())
-                time.sleep(5)
-                u[1].close()
-                ServerConfig.clients[f].pop()
-                return f"[+] User: {user} successfully kicked from the net!\r\n"
-            f += 1
-        return f"[x] Error, Failed to kick user!\r\n"
+        # try:
+        print(f"{ServerConfig.clients[int(user)]}\r\n")
+        if socket == ServerConfig.clients[int(user)][1]:
+            socket.send("[x] Error, You cannot kick yourself dummy!\r\n".encode())
+        else:
+            try:
+                ServerConfig.clients[int(user)][1].send(str(f"{Strings.MainColors['Clear']} {Strings.MainColors['Red']}\r\n\r\n\r\n\r\nYou just got kick by an admin!").encode())
+                ServerConfig.clients[int(user)][1].close()
+            except:
+                socket.send(str(f"[x] Error, Failed to kick user!\r\n").encode())
+
+            try:
+                ServerConfig.clients.pop(int(user))
+            except:
+                socket.send("[x] Error, Unable to remove element from array".encode())
+
+            socket.send(str(f"[+] User: {user} successfully kicked from the net!\r\n").encode())
+        # except:
+            # socket.send(str(f"[x] Error, Failed to kick user!\r\n").encode())
+
+    def boardcastmsg(socket, msg):
+        for usr in ServerConfig.clients:
+            if usr[1] != socket:
+                usr[1].send(str(f"\r\n[NEW MESSAGE FROM ADMIN]\r\n{msg}\r\n[~]════[Wocky]══$ ").encode())
+        socket.send("[+] Boardcast message successfully sent!\r\n".encode())
+
+
         
