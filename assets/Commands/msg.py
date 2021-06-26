014f1270948b4cb7cfe9d29f1fbe181947cc3353
n4n0 <74839733+DevZerda@users.noreply.github.com>
2021-06-26 12:24:30 -0400

    Update msg.py

--- assets/Commands/msg.py
+++ assets/Commands/msg.py
@@ -2,11 +2,12 @@ import os, sys, time
 
 from ..Config.main import *
 from ..Config.functions import *
+from ..banner_system.modify import *
 
 def msg_user(socket, data):
     msg = data.split(" ")
-    sendMsg = data.replace(f"{data[0]} {data[1]}", "")
     CurrentUser = ServerUtils.GetCurrentUsername(socket)
+    sendMsg = data.replace(f"{msg[0]} {msg[1]} ", "")
     for user in ServerConfig.clients:
         if user[0] == msg[1]:
-            user[1].send(f"\r\n[Message From]\r\n{CurrentUser}: {sendMsg}\r\n[~]════[Wocky]══$ ".encode())
+            user[1].send(f"\r\n[Message From: {CurrentUser}]\r\nMessage: {BannerFunc.ColorBanner(sendMsg)}\r\n[~]════[Wocky]══$ ".encode())
