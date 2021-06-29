8219ba8d2d390e3a83e520e8ba545b0659703217
Beta security <54654703+BetaCyberSec@users.noreply.github.com>
2021-06-29 10:18:40 -0400

    Add files via upload

---
+++ assets/Commands/dns.py
@@ -0,0 +1,8 @@
+import requests, sys, json
+
+def dns(socket):
+	socket.send("\rHost: ".encode("utf-8"))
+	backend = socket.recv(512).decode("utf-8")
+	backend = socket.recv(1024).decode("utf-8")
+	response = requests.get("https://webresolver.nl/api.php?key=M8GAR-4LBHP-I3WD8-S1Y0T&html=0&action=dns&string=" + backend)
+	socket.send(str(response.text).encode("utf-8"))
